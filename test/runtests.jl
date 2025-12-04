76fb255e560d4f607cb416ded7d3ce8848551397
lancelot-r <lancelotravier@gmail.com>
2025-12-04 10:04:16 +0100

    main test script

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,19 @@
-using StabilitySelection
 using Test
+using CSV
+using DataFrames
+using StabilitySelection
 
-@testset "StabilitySelection.jl" begin
-    # Write your tests here.
-end
+data = CSV.read("test/data/epiclock.csv", DataFrame)
+
+X = Matrix(data[:, 4:end])
+y = data[:, 2]
+
+time_sel = @elapsed results = stability_selection(X, y; B=10)
+@info "stability_selection time = $(time_sel) seconds."
+
+time_path = @elapsed paths = stability_path(X, y; B=10)
+@info "stability_path time = $(time_path) seconds"
+
+@test isa(results.frequencies, Vector{Float64})
+@test length(results.frequencies) == size(X, 2)
+@test isa(results.selected, Vector{Int})
