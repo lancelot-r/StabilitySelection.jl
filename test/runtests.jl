60920d7257a147b3531317778a2b67192c3a88e8
lancelot-r <lancelotravier@gmail.com>
2025-12-17 17:16:08 +0100

    adding R time process

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,10 +9,10 @@ X = Matrix(data[:, 4:end])
 y = data[:, 2]
 
 time_sel = @elapsed results = stability_selection(X, y; B=10)
-@info "stability_selection time = $(time_sel) seconds."
+@info "stability_selection time = $(time_sel) seconds, R time : 107.3920 seconds."
 
 time_path = @elapsed paths = stability_path(X, y; B=10)
-@info "stability_path time = $(time_path) seconds"
+@info "stability_path time = $(time_path) seconds, R time : 103.5061 seconds."
 
 @test isa(results.frequencies, Vector{Float64})
 @test length(results.frequencies) == size(X, 2)
